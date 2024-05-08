36694a5c8167381bb397f3df68df48bbcac3c529
„JohannHelbert <johannhelbert@gmail.com>
2024-05-08 09:54:07 +0200

    Creating our first store

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+import TodoForm from './TodoForm' import TodoList from './TodoList'
+<template>
+  <div class="todo-app">
+    <h1>To Do List</h1>
+    <todo-form />
+    <todo-list />
+  </div>
+</template>
+
+<style scoped></style>
