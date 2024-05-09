a024db067848f39b32e17798cf165c8f94c1e33d
„JohannHelbert <johannhelbert@gmail.com>
2024-05-09 12:17:00 +0200

    Add Mutating state with Actions

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import { useTodoListStore } from "@/stores/todoList";
+import { storeToRefs } from "pinia";
+
+const store = useTodoListStore();
+
+// storeToRefs lets todoList keep reactivity:
+const { todoList } = storeToRefs(store);
+</script>
 
 <template>
-  <div></div>
+  <div v-for="todo in todolist" :key="todo.id">
+    <div class="content">
+      <span>{{ todo.item }}</span>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
