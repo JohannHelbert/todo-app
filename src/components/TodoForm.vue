4ec4157d465e9b746fada4265ce3aa0a209d6a28
„JohannHelbert <johannhelbert@gmail.com>
2024-05-09 12:46:16 +0200

    Add Reactive Properties in Pinia

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from "vue";
-import { useTodoListStore } from "@/stores/todoList";
+import { useTodoListStore } from "@/stores/todoList.js";
 
 const todo = ref("");
 const store = useTodoListStore();
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,14 +6,22 @@ const store = useTodoListStore();
 
 // storeToRefs lets todoList keep reactivity:
 const { todoList } = storeToRefs(store);
+
+// destructuring action method doesn't require using storeToRefs:
+const { toggleCompleted } = store;
 </script>
 
 <template>
-  <div v-for="todo in todolist" :key="todo.id">
-    <div class="content">
-      <span>{{ todo.item }}</span>
+  <div v-for="todo in todoList" :key="todo.id" class="list">
+    <div class="item">
+      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
